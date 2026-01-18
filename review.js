4268e4383456e8ccb08f00f37ba681b46af55f34
Tayalov <241752@astanait.edu.kz>
2026-01-18 18:25:16 +0500

    Create review.js

---
+++ review.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  text: { type: String, required: true },
+  rating: { type: Number, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Review', reviewSchema);
